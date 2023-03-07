1e7f79a3bf4f1dd768b824da8cefa661b3012469
Wild Mosquito <nailnh@code.edu.az>
2023-03-07 09:14:44 +0400

    task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿void Nzia(string[] arr)
+{
+    Console.WriteLine("Username: ");
+    string usr = Console.ReadLine();
+    Console.WriteLine("Password: ");
+    string pswrd = Console.ReadLine();
+    User user = new User();
+}
+
---
+++ Task2/User.cs
@@ -0,0 +1,39 @@
+﻿
+internal class User
+{
+    private string _username;
+    private string _password;
+    public string Username
+    {
+        get { return _username; }
+        set
+        {
+            if (!string.IsNullOrEmpty(value.Trim(' ')) && value.Length > 1 && value.Length < 50)
+            {
+                _username = value;
+            }
+            else
+            {
+                Console.WriteLine("Enter correct Username");
+            }
+        }
+    }
+    public string Password
+    {
+        get { return _password; }
+        set
+        {
+            if (!string.IsNullOrEmpty(value.Trim(' ')) && value.Length > 1 && value.Length < 50)
+            {
+                _password = value;
+            }
+            else
+            {
+                Console.WriteLine("Enter correct Password");
+            }
+        }
+    }
+
+
+
+}
